9f08c9c6b1caf07d3e74936a4cd2ed3ece1c2bde
Damian Lee <damian@tag1consulting.com>
2016-04-06 07:40:55 +0100

    Move plists code into plists.go file

--- main.go
+++ main.go
@@ -4,10 +4,6 @@ import (
   "github.com/codegangsta/cli"
   "os"
   "fmt"
-  "io/ioutil"
-  "os/user"
-  "path"
-  "regexp"
   "os/exec"
 )
 
@@ -79,93 +75,4 @@ func execute_command(command string, c *cli.Context) {
   }
 
   fmt.Print(string(out))
-}
-
-// Return a single plist file path to execute a command on.
-func single_filtered_plist(pattern string) string {
-  filtered_plists := filter_plists(pattern)
-
-  if len(filtered_plists) == 0 {
-    fmt.Println("No matches")
-    os.Exit(1)
-  }
-
-  if len(filtered_plists) > 1 {
-    fmt.Println("Too many matches")
-    os.Exit(1)
-  }
-
-  for _, plist := range filter_plists(pattern) {
-    return plist;
-  }
-
-  return ""
-}
-
-// Filter plist items by a pattern.
-func filter_plists(pattern string) map[string]string {
-  // Return all plists if pattern is empty.
-  if pattern == "" {
-    return plists()
-  }
-
-  filtered_plists := make(map[string]string)
-  regexp := regexp.MustCompile(pattern)
-
-  for k, v := range plists() {
-    if regexp.MatchString(k) {
-      filtered_plists[k] = v
-    }
-  }
-
-  return filtered_plists
-}
-
-// Get a list of plist files in each plist directory.
-func plists() map[string]string {
-  plists := make(map[string]string)
-
-  for _, dir := range dirs() {
-    // Check if the dir exists. If it doesn't, move on to the next.
-    if _, err := os.Stat(dir); err != nil {
-      continue
-    }
-
-    plist_files, err := ioutil.ReadDir(dir)
-
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
-
-    for _, file := range plist_files {
-      plists[file.Name()] = path.Join(dir, file.Name())
-    }
-  }
-
-  return plists
-}
-
-// Get a list of direcotries to search.
-func dirs() []string {
-  user, err := user.Current()
-
-  if err != nil {
-    fmt.Println(err)
-    os.Exit(1)
-  }
-
-  dirs := []string{"/Library/LaunchAgents", path.Join(user.HomeDir, "Library", "LaunchAgents")}
-
-  if is_root() {
-    root_dirs := []string{"/Library/LaunchDaemons", "/System/Library/LaunchDaemons"}
-    dirs = append(dirs, root_dirs ...)
-  }
-
-  return dirs
-}
-
-// Determine if the current process is running as root.
-func is_root() bool {
-  return os.Geteuid() == 0
 }
